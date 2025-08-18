33e0c0e63b78ed30cef3bb24631ce3454a28aa52
Тимур <@DESKTOP-LK5N1TP>
2025-08-18 20:52:45 +0300

    Print & Save DONE

--- Academe/main.cpp
+++ Academe/main.cpp
@@ -209,6 +209,7 @@ public:
 	std::ostream& info(std::ostream& os)const override
 	{
 		//return Human::info(os) << " " << speciality << " " << experience;
+		Human::info(os);
 		os.width(SPECIALITY_WIDTH);
 		os << speciality;
 		os.width(EXPERIENCE_WIDTH);
@@ -252,6 +253,38 @@ public:
 		return Student::info(os) << " " << get_subject();
 	}
 };
+
+void Print(Human* group[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		group[i]->info(cout);
+		
+		cout << delimiter << endl;
+	}
+	cout << "Количество объектов: " << group[0]->get_count() << endl;
+	cout << "Количество объектов: " << Human::get_count() << endl;
+
+
+}
+
+void Save(Human* group[], const int n, const std::string& filename)
+{
+	std::ofstream fout(filename);
+	for (int i = 0; i < n; i++)
+	{
+		
+		fout << *group[i] << endl;
+		
+	}
+
+
+	fout.close();
+	std::string cmd = "notepad";
+	cmd += filename;
+	system(cmd.c_str());
+
+}
 
 //#define INHERITANCE
 #define POLYMORPHISM //(poly - много, morphis - форма)
@@ -299,19 +332,8 @@ void main()
 		new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
 	};
 
-	std::ofstream fout("group.txt");
-	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
-	{
-		group[i]->info(cout);
-		fout << *group[i] << endl;
-		cout << delimiter << endl;
-	}
-
-	cout << "Количество объектов: " << group[0]->get_count() << endl;
-	cout << "Количество объектов: " << Human::get_count() << endl;
-
-	fout.close();
-	system("notepad group.txt");
+	Print(group, sizeof(group) / sizeof(group[0]));
+	Save(group, sizeof(group) / sizeof(group[0]), "group.txt");
 
 	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
 	{
