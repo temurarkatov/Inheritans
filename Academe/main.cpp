c07c240c9d280dc2d5f1f688f86156bdf214257b
Тимур <@DESKTOP-LK5N1TP>
2025-08-20 21:13:08 +0300

    Load DONE

--- Academe/main.cpp
+++ Academe/main.cpp
@@ -82,6 +82,10 @@ public:
 		os << age;
 		return os;
 	}
+	virtual std::istream& scan(std::istream& is)
+	{
+		return is >> last_name >> first_name >> age;
+	}
 };
 
 int Human::count = 0;
@@ -89,6 +93,11 @@ int Human::count = 0;
 std::ostream& operator<<(std::ostream& os, const Human& obj)
 {
 	return obj.info(os);
+}
+
+std::istream& operator >> (std::istream& is, Human& obj)
+{
+	return obj.scan(is);
 }
 
 #define STUDENT_TAKE_PARAMETERS const std::string& speciality, const std::string& group, double rating, double attendance
@@ -166,6 +175,22 @@ public:
 		os << attendance;
 		return os;
 	}
+	std::istream& scan(std::istream& is)override
+	{
+		Human::scan(is);
+		char sz_buffer[SPECIALITY_WIDTH + 1] = {};
+		is.read(sz_buffer, SPECIALITY_WIDTH);
+		
+		for (int i = SPECIALITY_WIDTH - 1; sz_buffer[i] == ' '; i--)sz_buffer[i] = 0;
+		while (sz_buffer[0] == ' ')
+			for (int i = 0; sz_buffer[i]; i++)sz_buffer[i] = sz_buffer[i + 1];
+
+		speciality = sz_buffer;
+
+		is >> group >> rating >> attendance;
+		return is;
+
+	}
 };
 
 #define TEACHER_TAKE_PARAMETERS const std::string& speciality, int experience
@@ -217,6 +242,22 @@ public:
 		return os;
 
 	}
+	std::istream& scan(std::istream& is)override
+	{
+		Human::scan(is);
+		char sz_buffer[SPECIALITY_WIDTH + 1] = {};
+		is.read(sz_buffer, SPECIALITY_WIDTH);
+
+		for (int i = SPECIALITY_WIDTH - 1; sz_buffer[i] == ' '; i--)sz_buffer[i] = 0;
+		while (sz_buffer[0] == ' ')
+			for (int i = 0; sz_buffer[i]; i++)sz_buffer[i] = sz_buffer[i + 1];
+
+		speciality = sz_buffer;
+
+		is >> experience;
+		return is;
+
+	}
 };
 
 #define GRADUATE_TAKE_PARAMETERS const std::string& subject
@@ -252,6 +293,13 @@ public:
 	{
 		return Student::info(os) << " " << get_subject();
 	}
+	std::istream& scan(std::istream& is)override
+	{
+		Student::scan(is);
+		std::getline(is, subject);
+		return is;
+	}
+
 };
 
 void Print(Human* group[], const int n)
@@ -286,6 +334,16 @@ void Save(Human* group[], const int n, const std::string& filename)
 
 }
 
+Human* HumanFactory(const std::string& type)
+{
+	Human* human = nullptr;
+	if (strstr(type.c_str(), "Human"))human = new Human("", "", 0);
+	else if (strstr(type.c_str(), "Student"))human = new Student("", "", 0, "", "", 0, 0);
+	else if (strstr(type.c_str(), "Graduate"))human = new Graduate("", "", 0, "", "", 0, 0, "");
+	else if (strstr(type.c_str(), "Teacher"))human = new Teacher("", "", 0, "", 0);
+	return human;
+}
+
 Human** Load(const std::string& filename, int& n)
 {
 	Human** group = nullptr;
@@ -310,6 +368,16 @@ Human** Load(const std::string& filename, int& n)
         fin.seekg(0);
 		
 		cout << "Position" << fin.tellg() << endl;
+
+		for (int i = 0; !fin.eof(); )
+		{
+			std::string buffer;
+			std::getline(fin, buffer, ':');
+			if (buffer.size() < 5)continue;
+			group[i] = HumanFactory(buffer);
+			fin >> *group[i];
+			i++;
+		}
 	}
 	else
 	{
